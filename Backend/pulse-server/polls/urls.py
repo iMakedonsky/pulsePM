6a5f66f360ba819946beb6814b45b78a8d8a15cf
GlebVollodin <glebvollodin@gmail.com>
2026-08-08 23:57:21 +0200

    tutoriing: last is admin fields design 7 part

--- Backend/pulse-server/polls/urls.py
+++ Backend/pulse-server/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path('hello/', views.index, name='index'),
-    path('', views.home, name='home'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='details'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
