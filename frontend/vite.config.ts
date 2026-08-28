a367156edf2b8427d1702a1a0024b1c382d6ad16
Alex Mashianov <omashianov@intersog.com>
2026-08-27 22:44:27 -0400

    add initial frontend template

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    // netlify(), disable this plugin until we start deploying
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
