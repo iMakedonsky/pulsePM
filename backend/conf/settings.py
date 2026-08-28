a2bf4a0ffeeae1da96e726e7f541a7565c57fb75
Alex Mashianov <omashianov@intersog.com>
2026-08-28 00:29:54 -0400

    remove obsolete directory

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'pulse' / 'templates'],
+        'DIRS': [BASE_DIR / 'organizations' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'pulse' / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'organizations' / 'static']
 
 AUTH_USER_MODEL = 'users.User'
 
